305bef10933c6fc9870f0c14f3bef68075a3be6d
overlord <overlord7wizard@gmail.com>
2017-11-30 00:23:39 +0200

    base

--- main.c
+++ main.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+void menu (void);
+void add_book (void);
+void exit (void);
+void addmem (void);
+void delete_book (void);
+void borrow_book (void);
+void search_book (void);
+void admin (void);
+void user (void);
+void overdue(void);
+void popular_book (void);
+
+typedef struct
+{
+    char catg[32];
+    char title[32];
+    char author[32];
+    int isbn;
+    int no_cpy;
+    char publisher[32];
+}books;
+
+typedef struct
+{
+    char f_name[10];
+    char l_name[10];
+    int id;
+    int age;
+    char email[64];
+    int street_no;
+    char zone[20];
+    char city[20];
+}users;
 
 int main()
 {
-    printf("Hello world!\n");
+    menu();
     return 0;
+}
+
+void menu (void)
+{
+
+    switch(getch())
+    {
+        case '1':break;
+        default:
+    }
 }
