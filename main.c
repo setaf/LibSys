6c460b8032ea83a95361d398cd5d8ec7470c72dd
overlord <overlord7wizard@gmail.com>
2017-11-30 21:17:05 +0200

    test4

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 #include <time.h>
 #include <conio.h>
 
+//GLOBAL DECLERTAION//
+FILE *fb;
+time_t borr_date,ret_date;
+int k;
+
+void del_mem(void);
 void menu (void);
 void add_book (void);
-void exit (void);
+void exit_prog (void);
 void addmem (void);
 void delete_book (void);
 void borrow_book (void);
@@ -14,6 +20,7 @@ void admin (void);
 void user (void);
 void overdue(void);
 void popular_book (void);
+void ret_book(void);
 
 typedef struct
 {
@@ -23,6 +30,7 @@ typedef struct
     int isbn;
     int no_cpy;
     char publisher[32];
+
 }books;
 
 typedef struct
@@ -35,7 +43,10 @@ typedef struct
     int street_no;
     char zone[20];
     char city[20];
-}users;
+    int borr_no=0;
+    char ret_date [3][12];
+
+}*users;
 
 int main()
 {
@@ -45,10 +56,95 @@ int main()
 
 void menu (void)
 {
-
+    print("1-admin\n");
+    print("2-user\n");
     switch(getch())
     {
-        case '1':break;
+        case '1':admin();break;
+        case '2': addmem();break;
         default:
+            printf("Enter A valid Number");
+            menu();
     }
+}
+void user()
+{
+    printf("1-Borrow Book\n");
+    printf("2-Return Book\n");
+    printf("3-Search Book\n");
+    printf("4-menu\n");
+    switch(getch())
+    {
+        case '1': borrow_book();break;
+        case '2': ret_book();break;
+        case '3': search();break;
+        case '4': menu();break;
+        default:
+            printf("Enter A valid Number");
+            user();
+    }
+}
+void borrow_book()
+{
+    //check//
+    if(users.borr_no==3)
+        {
+        printf("You Can't Borrow in This Moment");
+        user();
+    }
+    else{
+    printf("Enter ISBN");
+    users.borr_no++;
+    time(&borr_date);
+    ctime(&borr_date);
+    printf("Enter The Return Date YYYY-MM-DD:");
+    scanf("%s",users.ret_date[users.borr_no]);
+    books.no_cpy--;//error//
+    printf("You Want Save (Y/N):");
+    switch(getchar())
+    {
+    case 'y' 'Y':
+    case 'n' 'N': user();
+    }
+    }
+
+}
+void ret_book(void)
+{
+    printf("Enter ISBN");
+    users.borr_no--;
+    books.no_cpy++;//error//
+    time(&ret_date);
+    ctime(&ret_date);
+    printf("You Want Save (Y/N):");
+    switch(getchar())
+    {
+    case 'y' 'Y':
+    case 'n' 'N': user();
+    }
+}
+
+void admin(void)
+{
+    print("1-add\n");
+    print("2-delete\n");
+    print("3-search\n");
+    print("4-overdue\n");
+    printf("5-delete mem\n");
+    print("6-menu\n");
+    add_book();
+    delete_book();
+    search();
+    overdue();
+    del_mem();
+    menu();
+}
+
+void addmem(void)
+{
+    printf("1-Current Member");
+    printf("2-New Member");
+    fp=fopen("members.txt","w");
+
+
 }
