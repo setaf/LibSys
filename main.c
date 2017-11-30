3d921e7e081996606d6cfac6522eaabd46e00df9
sloth <mohamed.hany556@gmail.com>
2017-11-30 14:32:35 +0200

    help

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <time.h>
 #include <conio.h>
 
+//GLOBAL DECLERTAION//
+FILE *fb
+time_t borr_date,ret_date;
+
 void menu (void);
 void add_book (void);
 void exit (void);
@@ -14,6 +18,7 @@ void admin (void);
 void user (void);
 void overdue(void);
 void popular_book (void);
+void ret_book(void);
 
 typedef struct
 {
@@ -23,6 +28,7 @@ typedef struct
     int isbn;
     int no_cpy;
     char publisher[32];
+
 }books;
 
 typedef struct
@@ -35,6 +41,9 @@ typedef struct
     int street_no;
     char zone[20];
     char city[20];
+    int borr_no=0;
+    char ret_date [3][12];
+
 }users;
 
 int main()
@@ -48,7 +57,61 @@ void menu (void)
 
     switch(getch())
     {
-        case '1':break;
+        case '1':admin();break;
+        case '2': user();break;
         default:
+            printf("Enter A valid Number");
+            menu();
+    }
+}
+void user()
+{
+    printf("1-Borrow Book\n2-Return Book");
+    switch(getch())
+    {
+        case '1': borrow_book();break;
+        case '2': ret_book();break;
+        default:
+            printf("Enter A valid Number");
+            user();
+    }
+}
+void borrow_book()
+{
+    //check//
+    if(users.borr_no==3)
+        {
+        printf("You Can't Borrow in This Moment");
+        user();
+    }
+    else{
+    printf("Enter ISBN");
+    users.borr_no++;
+    time(&borr_date);
+    ctime(&borr_date);
+    printf("Enter The Return Date YYYY-MM-DD:");
+    scanf("%s",users.ret_date[users.borr_no]);
+    books.no_cpy--;//error//
+    printf("You Want Save (Y/N):");
+    switch(getchar())
+    {
+    case 'y' 'Y':
+    case 'n' 'N': user();
+    }
+    }
+
+}
+void ret_book(void)
+{
+    printf("Enter ISBN");
+    users.borr_no--;
+    books.no_cpy++;//error//
+    time(&ret_date);
+    ctime(&ret_date);
+    printf("You Want Save (Y/N):");
+    switch(getchar())
+    {
+    case 'y' 'Y':
+    case 'n' 'N': user();
     }
 }
